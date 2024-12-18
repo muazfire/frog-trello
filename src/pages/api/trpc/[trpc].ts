64400f3d188d0731d701ff782319c1f80253b5b1
muazfire <68431482+muazfire@users.noreply.github.com>
2024-12-19 02:12:27 +0800

    Revert "Update [trpc].ts"
    This reverts commit d158ec0708ce834685640174cb56c45891fe732a.

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,28 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/routers";
 import { createContext } from "../../../server/context";
 
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
- 
-  if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "https://frog-trello.vercel.app/"); // Replace '*' with your production domain
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    return res.status(200).end();
-  }
-
-  // Pass the request to tRPC handler
-  return createNextApiHandler({
-    router: appRouter,
-    createContext,
-    onError:
-      process.env.NODE_ENV === "development"
-        ? ({ path, error }) => {
-            console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
-          }
-        : undefined,
-  })(req, res);
-}
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError:
+    process.env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+        }
+      : undefined,
+});
