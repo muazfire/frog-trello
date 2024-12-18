93c7ef06d48187be753724316a86d8d37f31338c
muazfire <68431482+muazfire@users.noreply.github.com>
2024-12-19 00:31:14 +0800

    remove clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 // pages/_app.tsx
 import { type AppType } from 'next/app';
-import { ClerkProvider } from '@clerk/nextjs';
 import { trpc } from '../utils/trpc';
 
 // Import your global CSS if you have any
-//import '../styles/globals.css';
+// import '../styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    
+    <Component {...pageProps} />
   );
 };
 
-// This line is crucial - it wraps our app with tRPC
 export default trpc.withTRPC(MyApp);
